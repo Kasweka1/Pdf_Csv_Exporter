17b92455bd71169a55c12cf92c235cf9946411e1
kasweka <kaswekamukoko@gmail.com>
2020-11-04 16:58:14 +0000

    added projecy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
-import 'mypdf.dart';
+import 'package:csv_reader/csv_reader.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-      home: MyPdf(),
-      debugShowCheckedModeBanner: false,
-    ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CSV App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String csvRaw;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: Text("Click Me"),
+            onPressed: () {
+              loadCSV();
+            },
+            color: Colors.deepOrange,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<String> loadAsset(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
+  void loadCSV() {
+    loadAsset('assets/SalaahTimes.csv').then((dynamic output) {
+      csvRaw = output;
+      print(csvRaw);
+    });
+  }
+}
