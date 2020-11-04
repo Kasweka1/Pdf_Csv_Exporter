b5f71902e4ebea1b29260bc1ed3900f7e7d0f05a
kasweka <kaswekamukoko@gmail.com>
2020-11-04 21:54:26 +0000

    made chnages

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:cp_convertor/mypdf.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,60 +18,43 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
   final pdf = pw.Document();
 
-  writeOnPdf(){
-    pdf.addPage(
-      pw.MultiPage(
-        pageFormat: PdfPageFormat.a5,
-        margin: pw.EdgeInsets.all(32),
-
-        build: (pw.Context context){
-          return <pw.Widget>  [
-            pw.Header(
-              level: 0,
-              child: pw.Text("PDF flutter Ecport")
-            ),
-
-            pw.Paragraph(
-              text: "This is an exprted flutter pdf file in the documents dorectory."
-            ),
-
-            pw.Paragraph(
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."
-            ),
-
-            pw.Header(
-              level: 1,
-              child: pw.Text("Second Heading")
-            ),
-
-            pw.Paragraph(
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."
-            ),
-
-            pw.Paragraph(
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."
-            ),
-
-            pw.Paragraph(
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."
-            ),
-          ];
-        },
-
-
-      )
-    );
+  writeOnPdf() {
+    pdf.addPage(pw.MultiPage(
+      pageFormat: PdfPageFormat.a5,
+      margin: pw.EdgeInsets.all(32),
+      build: (pw.Context context) {
+        return <pw.Widget>[
+          pw.Header(level: 0, child: pw.Text("PDF flutter Export")),
+          pw.Paragraph(
+              text:
+                  "This is an exprted flutter pdf file in the documents directory."),
+          pw.Paragraph(
+              text:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."),
+          pw.Header(level: 1, child: pw.Text("Second Heading")),
+          pw.Paragraph(
+              text:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."),
+          pw.Paragraph(
+              text:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."),
+          pw.Paragraph(
+              text:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."),
+        ];
+      },
+    ));
   }
 
-  Future savePdf() async{
+  Future savePdf() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
 
     String documentPath = documentDirectory.path;
@@ -84,7 +66,6 @@ class MyHomePage extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text("PDF Flutter"),
       ),
@@ -92,35 +73,25 @@ class MyHomePage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("PDF TUTORIAL", style: TextStyle(fontSize: 34),)
+            Text(
+              "Save Pdf on Floating ActionButton",
+              style: TextStyle(fontSize: 34),
+            )
           ],
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: ()async{
+        onPressed: () {
           writeOnPdf();
-          await savePdf();
-
-          Directory documentDirectory = await getApplicationDocumentsDirectory();
-
-          String documentPath = documentDirectory.path;
-
-          String fullPath = "$documentPath/exported_document.pdf";
-
-          Navigator.push(context, MaterialPageRoute(
-            builder: (context) => PdfPreviewScreen(path: fullPath,)
-          ));
-
+          //the save function has been called
+          savePdf();
         },
         child: Icon(Icons.save),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-
+      ), 
     );
   }
 }
