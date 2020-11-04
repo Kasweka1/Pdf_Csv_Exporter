3d1c5703f9236cf1d493b1ad09ebdd888ba10254
kasweka <kaswekamukoko@gmail.com>
2020-11-04 21:19:34 +0000

    b

--- lib/savecsv.dart
+++ lib/savecsv.dart
@@ -1,1 +1,10 @@
-import '';
+import 'package:path_provider/path_provider.dart';
+// Future savePdf() async{
+//     Directory documentDirectory = await getApplicationDocumentsDirectory();
+
+//     String documentPath = documentDirectory.path;
+
+//     File file = File("$documentPath/exported_document.pdf");
+
+//     file.writeAsBytesSync(pdf.save());
+//   }
