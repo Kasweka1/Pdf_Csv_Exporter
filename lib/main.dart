53285b903baffe084baa8641302cc8c571eff32b
kasweka <kaswekamukoko@gmail.com>
2020-11-16 22:11:53 +0000

    b

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "CSV App",
+      title: "PDF App",
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
